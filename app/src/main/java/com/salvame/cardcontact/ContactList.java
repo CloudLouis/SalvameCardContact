d79a49a9416fd5ffcafce0a8d0f6928773a7f3b6
User One <user.one@email.com>
2018-11-12 16:40:10 +0700

    Confirm addition of Medium's tutorial on NFC Reader. Untested

--- app/src/main/java/com/salvame/cardcontact/ContactList.java
+++ app/src/main/java/com/salvame/cardcontact/ContactList.java
@@ -4,7 +4,14 @@ import android.app.PendingIntent;
 import android.arch.persistence.room.Room;
 import android.content.Intent;
 import android.net.Uri;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
+import android.nfc.tech.MifareUltralight;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +24,7 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.nfc.NfcAdapter;
+import com.salvame.nfcreader.*;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
@@ -26,8 +34,11 @@ import org.json.JSONObject;
 
 import com.salvame.cardcontact.db.Database;
 import com.salvame.cardcontact.db.entity.ContactEntity;
+import com.salvame.nfcreader.parser.NdefMessageParser;
+import com.salvame.nfcreader.record.ParsedNdefRecord;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ContactList extends AppCompatActivity implements View.OnClickListener {
@@ -72,6 +83,34 @@ public class ContactList extends AppCompatActivity implements View.OnClickListen
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(this);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "No NFC", Toast.LENGTH_SHORT).show();
+        }
+
+        pendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, this.getClass())
+                        .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (nfcAdapter != null) {
+            if (!nfcAdapter.isEnabled())
+                showWirelessSettings();
+
+            nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+        }
+    }
+
+    private void showWirelessSettings() {
+        Toast.makeText(this, "You need to enable NFC", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
+        startActivity(intent);
     }
 
     //Getting the scan results
@@ -87,19 +126,8 @@ public class ContactList extends AppCompatActivity implements View.OnClickListen
                 try {
                     //converting the data to json
                     JSONObject obj = new JSONObject(result.getContents());
-                    //adding new values to database and reloading view
-                    database = Room.databaseBuilder(this, Database.class, "mainDB").fallbackToDestructiveMigration().allowMainThreadQueries().build();
-                    ContactEntity new_contact = new ContactEntity();
-                    new_contact.setName(obj.getString("name"));
-                    new_contact.setEmail(obj.getString("email"));
-                    new_contact.setPhone_number(obj.getString("phone_number"));
-                    new_contact.setCompany(obj.getString("company"));
-                    new_contact.setLine(obj.getString("line"));
-                    new_contact.setWhatsapp(obj.getString("whatsapp"));
-                    new_contact.setWebpage(obj.getString("web_link"));
-                    database.getContactDao().insert(new_contact);
 
-                    updateContactList();
+                    addContact(obj);
 
                     Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(obj.getString("web_link")));
                     startActivity(browserIntent);
@@ -131,6 +159,25 @@ public class ContactList extends AppCompatActivity implements View.OnClickListen
         database.getContactDao().delete(database.getContactDao().getContactEntityById(id));
         database.close();
         updateContactList();
+    }
+
+    public void addContact(JSONObject obj) throws JSONException {
+        //adding new values to database and reloading view
+        database = Room.databaseBuilder(this, Database.class, "mainDB").fallbackToDestructiveMigration().allowMainThreadQueries().build();
+        ContactEntity new_contact = new ContactEntity();
+        new_contact.setName(obj.getString("name"));
+        new_contact.setEmail(obj.getString("email"));
+        new_contact.setPhone_number(obj.getString("phone_number"));
+        new_contact.setCompany(obj.getString("company"));
+        new_contact.setLine(obj.getString("line"));
+        new_contact.setWhatsapp(obj.getString("whatsapp"));
+        new_contact.setWebpage(obj.getString("web_link"));
+        database.getContactDao().insert(new_contact);
+
+        updateContactList();
+
+        database.close();
+
     }
 
     public void updateContactList(){
@@ -171,5 +218,184 @@ public class ContactList extends AppCompatActivity implements View.OnClickListen
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //NFC READER PART OF THE CODE
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        resolveIntent(intent);
+    }
+
+    private void resolveIntent(Intent intent) {
+        String action = intent.getAction();
+
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs;
+
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+
+            } else {
+                byte[] empty = new byte[0];
+                byte[] id = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
+                Tag tag = (Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                byte[] payload = dumpTagData(tag).getBytes();
+                NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, id, payload);
+                NdefMessage msg = new NdefMessage(new NdefRecord[] {record});
+                msgs = new NdefMessage[] {msg};
+            }
+
+            displayMsgs(msgs);
+        }
+    }
+
+    private void displayMsgs(NdefMessage[] msgs) {
+        if (msgs == null || msgs.length == 0)
+            return;
+
+        StringBuilder builder = new StringBuilder();
+        List<ParsedNdefRecord> records = NdefMessageParser.parse(msgs[0]);
+        final int size = records.size();
+
+        for (int i = 0; i < size; i++) {
+            ParsedNdefRecord record = records.get(i);
+            String str = record.str();
+            builder.append(str);
+        }
+
+        Toast.makeText(this, builder.toString(), Toast.LENGTH_SHORT).show();
+
+    }
+
+    private String dumpTagData(Tag tag) {
+        StringBuilder sb = new StringBuilder();
+        byte[] id = tag.getId();
+        sb.append("ID (hex): ").append(toHex(id)).append('\n');
+        sb.append("ID (reversed hex): ").append(toReversedHex(id)).append('\n');
+        sb.append("ID (dec): ").append(toDec(id)).append('\n');
+        sb.append("ID (reversed dec): ").append(toReversedDec(id)).append('\n');
+
+        String prefix = "android.nfc.tech.";
+        sb.append("Technologies: ");
+        for (String tech : tag.getTechList()) {
+            sb.append(tech.substring(prefix.length()));
+            sb.append(", ");
+        }
+
+        sb.delete(sb.length() - 2, sb.length());
+
+        for (String tech : tag.getTechList()) {
+            if (tech.equals(MifareClassic.class.getName())) {
+                sb.append('\n');
+                String type = "Unknown";
+
+                try {
+                    MifareClassic mifareTag = MifareClassic.get(tag);
+
+                    switch (mifareTag.getType()) {
+                        case MifareClassic.TYPE_CLASSIC:
+                            type = "Classic";
+                            break;
+                        case MifareClassic.TYPE_PLUS:
+                            type = "Plus";
+                            break;
+                        case MifareClassic.TYPE_PRO:
+                            type = "Pro";
+                            break;
+                    }
+                    sb.append("Mifare Classic type: ");
+                    sb.append(type);
+                    sb.append('\n');
+
+                    sb.append("Mifare size: ");
+                    sb.append(mifareTag.getSize() + " bytes");
+                    sb.append('\n');
+
+                    sb.append("Mifare sectors: ");
+                    sb.append(mifareTag.getSectorCount());
+                    sb.append('\n');
+
+                    sb.append("Mifare blocks: ");
+                    sb.append(mifareTag.getBlockCount());
+                } catch (Exception e) {
+                    sb.append("Mifare classic error: " + e.getMessage());
+                }
+            }
+
+            if (tech.equals(MifareUltralight.class.getName())) {
+                sb.append('\n');
+                MifareUltralight mifareUlTag = MifareUltralight.get(tag);
+                String type = "Unknown";
+                switch (mifareUlTag.getType()) {
+                    case MifareUltralight.TYPE_ULTRALIGHT:
+                        type = "Ultralight";
+                        break;
+                    case MifareUltralight.TYPE_ULTRALIGHT_C:
+                        type = "Ultralight C";
+                        break;
+                }
+                sb.append("Mifare Ultralight type: ");
+                sb.append(type);
+            }
+        }
+
+        return sb.toString();
+    }
+    private String toHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = bytes.length - 1; i >= 0; --i) {
+            int b = bytes[i] & 0xff;
+            if (b < 0x10)
+                sb.append('0');
+            sb.append(Integer.toHexString(b));
+            if (i > 0) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+
+    private String toReversedHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; ++i) {
+            if (i > 0) {
+                sb.append(" ");
+            }
+            int b = bytes[i] & 0xff;
+            if (b < 0x10)
+                sb.append('0');
+            sb.append(Integer.toHexString(b));
+        }
+        return sb.toString();
+    }
+
+    private long toDec(byte[] bytes) {
+        long result = 0;
+        long factor = 1;
+        for (int i = 0; i < bytes.length; ++i) {
+            long value = bytes[i] & 0xffl;
+            result += value * factor;
+            factor *= 256l;
+        }
+        return result;
+    }
+
+    private long toReversedDec(byte[] bytes) {
+        long result = 0;
+        long factor = 1;
+        for (int i = bytes.length - 1; i >= 0; --i) {
+            long value = bytes[i] & 0xffl;
+            result += value * factor;
+            factor *= 256l;
+        }
+        return result;
     }
 }
---
+++ app/src/main/java/com/salvame/nfcreader/record/ParsedNdefRecord.java
@@ -0,0 +1,7 @@
+package com.salvame.nfcreader.record;
+
+public interface ParsedNdefRecord {
+
+    String str();
+
+}
